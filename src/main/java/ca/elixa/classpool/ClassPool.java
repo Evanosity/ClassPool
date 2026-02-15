877d29b4025cb7d6d26ebed79ac7a732c0123598
Evanosity <iamevanosity@gmail.com>
2026-02-15 00:23:14 -0500

    finish removal of reflections library; properly analyze jar at runtime

--- src/main/java/ca/elixa/classpool/ClassPool.java
+++ src/main/java/ca/elixa/classpool/ClassPool.java
@@ -1,20 +1,9 @@
 package ca.elixa.classpool;
 
-import org.reflections.Reflections;
-
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.stream.Stream;
-
-import static org.reflections.scanners.Scanners.SubTypes;
-import static org.reflections.scanners.Scanners.TypesAnnotated;
 
 /**
  * Given a package, scan the package and instantiate each of those classes. The inheritors will handle how to index
@@ -51,89 +40,51 @@ public abstract class ClassPool<T, I> {
         String cp = System.getProperty("java.class.path");
 
         try{
+
             JarFile jarFile = new JarFile(cp);
             Enumeration<JarEntry> e = jarFile.entries();
 
-            URL[] urls = { new URL("jar:file:" + cp + "!/") };
-            URLClassLoader cl = URLClassLoader.newInstance(urls);
-
             while (e.hasMoreElements()) {
+
                 JarEntry je = e.nextElement();
-                if (je.isDirectory() || !je.getName().endsWith(".class")) {
+
+                String jeName = je.getName();
+
+                if (je.isDirectory() || !jeName.endsWith(".class")) {
                     continue;
                 }
 
                 // Convert the file path to a class name (e.g., "pkg/MyClass.class" to "pkg.MyClass")
-                String className = je.getName().substring(0, je.getName().length() - ".class".length());
+                String className = jeName.substring(0, jeName.length() - 6);
                 className = className.replace('/', '.');
 
+                if(! className.startsWith(path))
+                    continue;
+
                 // Load the class
-                Class c = cl.loadClass(className);
-                System.out.println("Loaded class: " + c.getName());
+                Class<?> c = Class.forName(className);
+
+                if(! c.isAssignableFrom(type) || ! c.isAnnotationPresent(Indexed.class))
+                    continue;
+
+                Constructor<? extends T> cons = (Constructor<? extends T>) c.getDeclaredConstructor();
+                //make sure its accessible
+                cons.setAccessible(true);
+
+                var instance = cons.newInstance();
+
+                var i = indexHandler(instance, c, path);
+
+                index.put(i, instance);
             }
-            jarFile.close(); // Important to close the JarFile
-        }
-        catch(Exception e){
 
-        }
+            jarFile.close(); // Important to close the JarFile (thanks gemini)
 
-    }
-
-    /**
-     * This method is recursive. It will search all sub-packages.
-     * @param path - the dot delimited path of the package we are searching.
-     * @param baseType - the base class we are searching for
-     * @param files - the files we are searching
-     */
-    private void indexClasses(String path, Class<T> baseType, File... files) {
-        try{
-            for(File file : files){
-                String fileName = file.getName();
-
-                //if the given file is a directory, index all the classes inside of it.
-                if(file.isDirectory())
-                    indexClasses(path + "." + fileName, baseType, file.listFiles());
-
-                //otherwise, if its a class file, load er up.
-                else if(fileName.endsWith(".class")){
-                    String fullClassName = path + "." + fileName.substring(0, fileName.length() - 6);
-
-                    Class<?> loadedClass = Class.forName(fullClassName);
-
-                    //if the indexed annotation isn't present, ignore the class
-                    if(! loadedClass.isAnnotationPresent(Indexed.class))
-                        continue;
-
-                    //if the loaded class isn't a child class of the base type, ignore the class
-                    if(! baseType.isAssignableFrom(loadedClass))
-                        continue;
-
-
-
-                    //index it
-                    //I i = indexHandler(instance, loadedClass, path);
-                    //index.put(i, instance);
-                }
-            }
         }
         catch(Exception e){
             throw new RuntimeException("Fatal error while indexing " + path, e);
         }
-    }
 
-    private T instantiate(Class<T> clazz){
-
-        try{
-            //Get the constructor for the class
-            Constructor<? extends T> cons = (Constructor<? extends T>) clazz.getDeclaredConstructor();
-            //make sure its accessible
-            cons.setAccessible(true);
-
-            return cons.newInstance();
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     /**
